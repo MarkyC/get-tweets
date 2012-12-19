e618f3f3cfaedce9114f1bffd13d967c2023a758
Marco Cirillo <mmmarcooo@gmail.com>
2012-12-18 22:09:07 -0500

    Initial Commit

---
+++ src/name/marcocirillo/GetTweets.java
@@ -0,0 +1,12 @@
+package name.marcocirillo;
+
+public class GetTweets {
+
+	/**
+	 * @param args - Not used
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+
+}
