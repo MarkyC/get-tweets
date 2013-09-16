00fc34599645f3f3fd0ed3a22c95944f5e8e74c1
Andrea Guarini <guarini.andrea@hotmail.it>
2013-09-16 11:10:24 +0200

    Added more printable data, change output to CSV format so it can be imported in Excel

--- src/name/marcocirillo/Constants.java
+++ src/name/marcocirillo/Constants.java
@@ -19,14 +19,19 @@ public class Constants {
 	public static final String REMOVE_CONVERSATION_TEXT = "Remove conversations";
 	public static final String REMOVE_LINK_TEXT = "Remove tweets with links";
 	public static final String PRINT_TIME_TEXT = "Print time";
+	public static final String IN_REPLY_TO_SCREEN_NAME_TEXT = "Print in reply to screen name";
+	public static final String IN_REPLY_TO_STATUS_ID_TEXT = "Print in reply to status ID";
+	public static final String IN_REPLY_TO_USER_ID_TEXT = "Print in reply to user ID";
+	public static final String SOURCE_TEXT = "Print source";
+	public static final String STATUS_ID_TEXT = "Print status ID";
+	public static final String TEXT_TEXT = "Print text";	
 	
-	public static final int MAX_STATUSES = 3200;	// Max twitter statuses we can retrieve is 3200
+	public static final int MAX_STATUSES = 3500;	// Max twitter statuses we can retrieve is 3200
 	public static final int STATUS_PER_PAGE = 100;	// 100 statuses per page (100 * 32 = 3200)
 
 	public static final String CRASH_WINDOW_TITLE = "Crash Report";
 	
 	public static final String DOWNLOADING_TEXT = "Download tweets for user";
-
-	
+	public static final String DELIMITER_TEXT = "Set delimiter";
 
 }
